361ba72f9d237306cad45c7fda6cd7e63fe570c8
saiweekend <79636163+saiweekend@users.noreply.github.com>
2023-07-29 02:12:00 -0500

    Changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/index.html"],
+  content: ["./src/*.{html,js}"],
   theme: {
     extend: {},
   },
   plugins: [],
 }
-
